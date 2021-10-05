af6604889e9b3ab3c81fbdd63b8db2de6278cb70
MyaFryckman22 <817258@mystma.org>
2021-10-05 14:41:01 -0500

    2

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #WRITE YOUR CODE IN THIS FILE
+#define function 
+def hasL(w): 
+    w == w.lower()
+    for i in range(0, len(w)):
+        if (w[i]) == "l":
+            return True
+        else:
+            return False
+             
+hasL("dog") 
